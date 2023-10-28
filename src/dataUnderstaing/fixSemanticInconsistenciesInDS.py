a04575af71643328370f24e7a4b443f9a857e8fe
Sara Hoxha <sarahoxha99@gmail.com>
2023-10-28 22:46:10 +0200

    updated dataset with no incosistencies

--- src/dataUnderstaing/fixSemanticInconsistenciesInDS.py
+++ src/dataUnderstaing/fixSemanticInconsistenciesInDS.py
@@ -1,17 +1,19 @@
 import pandas as pd
 
-df = pd.read_csv('dataset (missing + split)/train.csv')
-file_name = ''
+df = pd.read_csv('dataset (missing + split)/Dataset_nomissing_nofeatur_noutlier.csv')
+file_name = 'dataset (missing + split)/Dataset_nomissing_nofeatur_noutlier_noinconsistencies.csv'
 
 mistake2correctname = {
     '100% of Disin U - Remixes' : '100% of Dissin You Remixes',
     '100% of Disin You - Remixes' : '100% of Dissin You Remixes',
     '100 % of Disin U - Remixes' : '100% of Dissin You Remixes',
+    '100% of Disin\' You': '100% of Dissin You',
     'Back On a Mission': 'Back On A Mission',
     'Voz d\'Amor' : 'Voz D\' Amor',
     'Front by Front' : 'Front By Front',
-    'I\'m Lovin\' It Remixes': 'I\'m Lovin\' It Remixed',
+    'I\'m Lovin\' It Remixed': 'I\'m Lovin\' It Remixes',
     'J\'adore Hardcore' : 'J\'Adore Hardcore',
+    'J\'adore Hardcore - Extended Mix': 'J\'Adore Hardcore - Extended Mix',
     'Music For a Big Night Out': 'Music for A Big Night Out'
 }
 df_copy = df.copy()
