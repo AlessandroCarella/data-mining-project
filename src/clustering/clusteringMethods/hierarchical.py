37450c3a3d2bd44b58fffccd9bccc058ffacb5b1
RafaelUrbina <riurbinah@gmail.com>
2023-11-06 22:28:07 +0100

    commit models

--- src/clustering/clusteringMethods/hierarchical.py
+++ src/clustering/clusteringMethods/hierarchical.py
@@ -3,10 +3,24 @@ import os.path as path
 
 from clusteringMethods.clusteringUtility import columnAlreadyInDf, copyAndScaleDataset, getMidRunObjectFolderPath, saveMidRunObjectToFile
 
-def hierarchical(df, originalDatasetColumnsToUse):
+def hierarchicalCentroidLinkage(df, columnsToUse,linkage_thresholds=[1, 100]):
     #@RafaelUrbina
-    # TODO: Implement the hierarchical method
-    return df, ["hierarchical"]
+    tempDfScal = copyAndScaleDataset (df, columnsToUse)
+
+    # Calculate the linkage matrix using centroid linkage
+    linkage_matrix = linkage(tempDfScal, method='centroid')
+    
+    columnsNames = []
+    for threshold in linkage_thresholds:
+        newColumnName = 'hierarchicalCentroidLinkage' + ' '  + 'threshold' + str(threshold)
+        columnsNames.append(newColumnName)
+        if not columnAlreadyInDf(newColumnName, df):
+            # Determine the cluster assignments using a distance or number of clusters threshold
+            clusters = fcluster(linkage_matrix, threshold, criterion='distance')
+            # Add the cluster assignments to the dataset
+            df[newColumnName] = clusters
+
+    return df, columnsNames
 
 def hierarchicalCompleteLinkage(df, columnsToUse, linkage_thresholds=[1, 100]):
     #@SaraHoxha
@@ -29,10 +43,24 @@ def hierarchicalCompleteLinkage(df, columnsToUse, linkage_thresholds=[1, 100]):
     return df, columnsNames
 
 
-def hierarchicalSingleLink(df, originalDatasetColumnsToUse):
+def hierarchicalSingleLinkage(df, columnsToUse, linkage_thresholds=[1, 100]):
     #@RafaelUrbina
-    # TODO: Implement the hierarchicalSingleLink method
-    return df, ["hierarchicalSingleLink"]
+    tempDfScal = copyAndScaleDataset (df, columnsToUse)
+
+    # Calculate the linkage matrix using single linkage
+    linkage_matrix = linkage(tempDfScal, method='single')
+    
+    columnsNames = []
+    for threshold in linkage_thresholds:
+        newColumnName = 'hierarchicalSingleLinkage' + ' '  + 'threshold' + str(threshold)
+        columnsNames.append(newColumnName)
+        if not columnAlreadyInDf(newColumnName, df):
+            # Determine the cluster assignments using a distance or number of clusters threshold
+            clusters = fcluster(linkage_matrix, threshold, criterion='distance')
+            # Add the cluster assignments to the dataset
+            df[newColumnName] = clusters
+            
+    return df, columnsNames
 
 # threshold: The distance threshold for clustering. Data points with distances below thisthreshold will be assigned to the same cluster.
 def hierarchicalGroupAverage(df, columnsToUse, criterion, thresholds=[1, 100]):
